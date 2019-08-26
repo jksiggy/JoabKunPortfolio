92af69837c9f1355d59d56b91ba698b51bc5370b
joab sigrah <jksiggy@gmail.com>
2019-08-25 19:08:38 -0500

    update

---
+++ js/portfolio.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener("click", function () {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
+
+// Smooth Anchor Scrolling
+$(document).on("click", 'a[href^="#"]', function (event) {
+  event.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    500
+  );
+});
+
+// When the user scrolls down 20px from the top of the document, show the scroll up button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("toTop").style.display = "block";
+  } else {
+    document.getElementById("toTop").style.display = "none";
+  }
+}
+
+// Preloader
+$(document).ready(function ($) {
+  $(".preloader-wrapper").fadeOut();
+  $("body").removeClass("preloader-site");
+});
+$(window).load(function () {
+  var Body = $("body");
+  Body.addClass("preloader-site");
+});
+
+
+function submitForm() {
+  // Get the first form with the name
+  // Hopefully there is only one, but there are more, select the correct index
+  var frm = document.getElementsByName('contact-form')[0];
+  frm.submit(); // Submit
+  frm.reset();  // Reset
+  return false; // Prevent page refresh
+}
+
